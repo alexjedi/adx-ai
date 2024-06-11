6db760160f9b93c50b4941a90c18029676b17794
Alex Shevlyakov <pxl.alexjedi@gmail.com>
2024-06-11 10:58:33 +0100

    Update to min chromium version

--- src/app/api/tv-scrapper/route.ts
+++ src/app/api/tv-scrapper/route.ts
@@ -1,15 +1,18 @@
 // @ts-nocheck
 
 import { NextResponse } from 'next/server'
-import puppeteerCore from 'puppeteer-core'
-import puppeteer from 'puppeteer'
-import chromium from '@sparticuz/chromium'
-
 export const dynamic = 'force-dynamic'
+
+const CHROMIUM_PATH =
+  'https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar'
 
 async function getBrowser() {
   if (process.env.VERCEL_ENV === 'production') {
-    const executablePath = await chromium.executablePath()
+    const chromium = await import('@sparticuz/chromium-min').then((mod) => mod.default)
+
+    const puppeteerCore = await import('puppeteer-core').then((mod) => mod.default)
+
+    const executablePath = await chromium.executablePath(CHROMIUM_PATH)
 
     const browser = await puppeteerCore.launch({
       args: chromium.args,
@@ -19,6 +22,8 @@ async function getBrowser() {
     })
     return browser
   } else {
+    const puppeteer = await import('puppeteer').then((mod) => mod.default)
+
     const browser = await puppeteer.launch()
     return browser
   }
